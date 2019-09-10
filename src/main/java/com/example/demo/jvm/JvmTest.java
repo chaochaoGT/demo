331a92f1d26c26752aabda2721fa590d6ad2055b
wangchao <wangchao@hellogeek.com>
2019-09-10 15:14:52 +0800

    jvm

--- src/main/java/com/example/demo/jvm/JvmTest.java
+++ src/main/java/com/example/demo/jvm/JvmTest.java
@@ -11,6 +11,28 @@ package com.example.demo.jvm;
 public class JvmTest {
 
     public static void main(String[] args) {
+     //testJVM_1();
+        //
+        testStack();
+
+    }
+
+    private static void testStack() {
+        long timeMillis = System.currentTimeMillis();
+        for (int i = 0; i < 100000000; i++) {
+            alloc();        
+        }
+        long timeMillis1 = System.currentTimeMillis();
+        System.out.println(timeMillis1-timeMillis);
+    }
+
+    private static void alloc() {
+        byte[] bytes=new byte[2];
+        bytes[0]=1;
+    }
+
+    //-6的二进制
+    private static void testJVM_1() {
         int a=-6;
         for (int i = 0; i < 32; i++) {
             int x=(a& 0x80000000>>>i)>>>(31-i);
